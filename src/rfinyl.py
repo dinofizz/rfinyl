bab0806df99f426b906e802f2ea47e7ddf1dc76d
Dino Fizzotti <fizzotti@gmail.com>
2014-03-05 21:37:55 +0200

    Added argument parsing so that the script can perform different actions

--- src/rfinyl.py
+++ src/rfinyl.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import argparse
 import sys
 import random
 import time
@@ -5,42 +7,6 @@ from mpd import MPDClient
 
 sys.path.append('../../../bzr/nfcpy'); #edit to point to nfcy directory
 import nfc
-
-
-# assume switch moved to play position
-# need to start reading and wait for a tag, timeout 5s
-# if we find a tag and read the 5 digit number successfully
-#   Search mpd playlists for playlist containing same id
-#   if found matching id
-#       begin playback
-#   else
-#       playlist not found error
-# else if tag read failure
-#   tag read error
-# else if timeout
-#   read timeout error
-
-# how to determine connectivity? call connect and see result?
-
-def connected(tag):
-    print tag.ndef.message.pretty() if tag.ndef else "Sorry, no NDEF"
-    rfinyl_id = nfc.ndef.TextRecord(tag.ndef.message[0]).text if tag.ndef else "Sorry, no NDEF"
-    return rfinyl_id.decode('utf-8')
-    #print rfinyl_id
-    ##id = random.randint(10000,99999)
-    ##tag.ndef.message = nfc.ndef.Message(nfc.ndef.TextRecord(language="en", text=id))
-
-    #mpd_client.connect("localhost", 6600)
-    #print(mpd_client.mpd_version)
-    #playlists = mpd_client.listplaylists()
-    #for playlist in playlists:
-    #    spotify_playlist = playlist['playlist'].decode('utf-8')
-    #    print spotify_playlist
-    #    #print dir(playlist)
-    #    if rfinyl_id in spotify_playlist:
-    #        mpd_client.load(rfinyl_id)
-    #        mpd_client.play()
-    #        print "MATCH"
             
 
 def error(error):
@@ -61,7 +27,7 @@ def play(mpd_client, rfinyl_id):
     error('Could not find playlist %s' % rfinyl_id)
 
 
-def read(mpd_client, clf):
+def read_tag(mpd_client, clf):
     after5s = lambda: time.time() - started > 5
     started = time.time()
 
@@ -77,14 +43,18 @@ def read(mpd_client, clf):
     else:
         play(mpd_client, rfinyl_id)
 
-def stop(mpd_client):
+
+def stop_playback(mpd_client):
     mpd_client.connect("localhost", 6600)
     mpd_client.clear()
 
 
-def new_playlist(clf):
+def write_tag(clf, write_id=None):
+    print write_id
+    playlist_file = 'current_playlists'
+    
     with open(playlist_file) as f:
-            return f.readlines()
+        current_playlists = f.readlines()
 
     after5s = lambda: time.time() - started > 5
     started = time.time()
@@ -99,29 +69,38 @@ def new_playlist(clf):
     except AttributeError:
         error("read timeout")
     else:
-        for attempt in range(10000, 100000):
-            new_id = random.randint(10000,99999)
-            if new_id not in current_playlists:
-                read_result.ndef.message = nfc.ndef.Message(nfc.ndef.TextRecord(language="en", text=new_id))
-                with open(playlist_file, 'a') as f:
-                    f.write(new_id)
-                return
-        error('playlist limit reached')
+        if write_id is None:
+            for attempt in range(10000, 100000):
+                new_id = random.randint(10000,99999)
+                if new_id not in current_playlists:
+                    read_result.ndef.message = nfc.ndef.Message(nfc.ndef.TextRecord(language="en", text=new_id))
+                    with open(playlist_file, 'a') as f:
+                        f.write(str(new_id))
+                    return
+            error('playlist limit reached')
+        else:
+            read_result.ndef.message = nfc.ndef.Message(nfc.ndef.TextRecord(language="en", text=write_id))
 
 
-def main():
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='rfinyl: streaming vinyl player')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-w','--write', help='Text to write to tage',required=False)
+    group.add_argument('-r', '--read', action='store_true', required=False)
+    group.add_argument('-n', '--new', action='store_true', required=False)
+    group.add_argument('-s', '--stop', action='store_true', required=False)
+    args = parser.parse_args()
+
     mpd_client = MPDClient()
     mpd_client.timeout = 10;
     mpd_client.idletimeout = None
-
-    #start mopidy client?
-
     clf = nfc.ContactlessFrontend('tty:USB0:pn53x')
 
-    read(mpd_client, clf)
-    #stop(mpd_client)
-    #new_playlist(clf)
-
-if __name__=="__main__":
-    main()
-
+    if args.read:
+        read_tag(mpd_client, clf)
+    elif args.new:
+        write_tag(clf)
+    elif args.write:
+        write_tag(clf, args.write)
+    elif args.stop:
+        stop_playback(mpd_client)
